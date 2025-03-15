f807e870623eef819c7939a347b421584fbbc647
cyril grinko <k.a.grinko@gmail.com>
2025-03-15 03:54:22 +0300

    sun solved

--- 02_data_storage_and_representation/15_sum.c
+++ 02_data_storage_and_representation/15_sum.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef long double ld;
+
+ld AddUpNumbers(ld lhs, ld rhs, ld *loss) {
+  ld sum = lhs + rhs;
+  *loss = rhs - (sum - lhs);
+  return sum;
+}
 
 int main() {
-    return 0;
+  ld sum = 0;
+  ld error = 0;
+  ld next_one = 0;
+  ld accumulated_error = 0;
+
+  while (scanf("%Lf", &next_one) != EOF) {
+    sum = AddUpNumbers(sum, next_one, &error);
+    accumulated_error += error;
+  }
+
+  printf("%.20Lf", sum + accumulated_error);
+
+  return EXIT_SUCCESS;
 }
