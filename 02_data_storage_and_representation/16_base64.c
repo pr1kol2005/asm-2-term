b7a6fd36ef662c4a62837ee88a7680680429eae2
cyril grinko <k.a.grinko@gmail.com>
2025-03-16 12:51:12 +0300

    2ns contest completed

--- 02_data_storage_and_representation/16_base64.c
+++ 02_data_storage_and_representation/16_base64.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned char byte;
+
+#define MAX_SIZE 100000
 
 int main() {
-    return 0;
+  FILE* input_file = fopen("data", "rb");
+  if (input_file == NULL) {
+    return EXIT_FAILURE;
+  }
+
+  FILE* output_file = fopen("base64", "w");
+  if (output_file == NULL) {
+    fclose(input_file);
+    return EXIT_FAILURE;
+  }
+
+  byte input_buffer[MAX_SIZE * 3] = {};
+  size_t output_buffer[MAX_SIZE] = {};
+  byte digits[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+  size_t read_count = 0;
+  while ((read_count = fread(input_buffer, 3, MAX_SIZE, input_file))) {
+    for (size_t i = 0; i < read_count; ++i) {
+      size_t combined = (input_buffer[i * 3] << 16) | (input_buffer[i * 3 + 1] << 8) | input_buffer[i * 3 + 2];
+
+      output_buffer[i] = digits[(combined >> 18) & 63];
+      output_buffer[i] |= digits[(combined >> 12) & 63] << 8;
+      output_buffer[i] |= digits[(combined >> 6) & 63] << 16;
+      output_buffer[i] |= digits[combined & 63] << 24;
+    }
+    fwrite(output_buffer, 4, read_count, output_file);
+  }
+
+  fclose(input_file);
+  fclose(output_file);
+
+  return EXIT_SUCCESS;
 }
--- 02_data_storage_and_representation/17_float.c
+++ 02_data_storage_and_representation/17_float.c
@@ -1,5 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void parse_fractional_number(const char* input_line) {
+  int whole_part = atoi(input_line);
+
+  const char* end_of_whole_part = input_line;
+  while (*end_of_whole_part && *end_of_whole_part != '.') {
+    end_of_whole_part++;
+  }
+
+  int numerator = 0;
+  int denominator = 1;
+
+  if (*end_of_whole_part == '.') {
+    end_of_whole_part++;
+    numerator = atoi(end_of_whole_part);
+
+    for (const char* ptr = end_of_whole_part; '0' <= *ptr && *ptr <= '9';
+         ptr++) {
+      denominator *= 10;
+    }
+
+    while (!(denominator % 2) && !(numerator % 2)) {
+      numerator /= 2;
+      denominator /= 2;
+    }
+
+    while (!(denominator % 5) && !(numerator % 5)) {
+      numerator /= 5;
+      denominator /= 5;
+    }
+  }
+
+  printf("%d (%d/%d)\n", whole_part, numerator, denominator);
+}
 
 int main() {
-    return 0;
+  char* input_line = NULL;
+  size_t input_line_size = 0;
+
+  if (getline(&input_line, &input_line_size, stdin) == -1) {
+    free(input_line);
+    return EXIT_FAILURE;
+  }
+
+  parse_fractional_number(input_line);
+
+  free(input_line);
+  return EXIT_SUCCESS;
 }
